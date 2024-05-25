dc51d51eaeae62d3f1e73cfa8f5627cb8cf1fbd6
Will McIntyre <william.d.j.mcintyre@gmail.com>
2024-05-25 10:58:31 -0400

    Update __init__.py

--- TMMC_Wrapper/__init__.py
+++ TMMC_Wrapper/__init__.py
@@ -43,7 +43,7 @@ import ultralytics
 from ultralytics import YOLO
 
 #---constants---
-CONST_speed_control = 1 #set this to 1 for full speed, 0.5 for half speed
+CONST_speed_control = 10 #set this to 1 for full speed, 0.5 for half speed
 DEBUG = False #set to false to disable terminal printing of some functions
 
 is_SIM = False #to disable some functions that can not be used on the sim
